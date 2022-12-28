a1e09db3398864f21773060735369faef564dfbe
Krystian Grzelak <krystian.grzelak@o2.pl>
2022-12-28 10:09:07 +0100

    feat: add material

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 import { environment } from '@env/environment';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 if (environment.production) {
   enableProdMode();
@@ -12,6 +13,6 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(RouterModule.forRoot(routes), HttpClientModule),
+    importProvidersFrom(RouterModule.forRoot(routes), HttpClientModule, BrowserAnimationsModule),
   ],
 }).catch((err) => console.error(err));
