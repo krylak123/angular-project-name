e9fb4fa8cec6b04823ea0e4b93df8e46d5583142
Krystian Grzelak <krystian.grzelak@o2.pl>
2022-12-29 18:11:44 +0100

    feat: create common routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,2 +1,24 @@
 import { Routes } from '@angular/router';
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'auth',
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./views/auth/auth.routes').then((r) => r.authRoutes),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./views/dashboard/dashboard.routes').then(
+        (r) => r.dashboardRoutes
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'auth',
+  },
+];
---
+++ src/app/views/auth/auth.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+export const authRoutes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./auth.component').then((c) => c.AuthComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
