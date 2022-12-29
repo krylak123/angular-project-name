c97fa20e123b2ffb84cc38760561d89f96469b22
Krystian Grzelak <krystian.grzelak@o2.pl>
2022-12-29 21:03:59 +0100

    feat: create loader component for lazy loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,48 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+  RouterModule,
+} from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, LoaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  private isLoading = new BehaviorSubject<boolean>(false);
+
+  protected get isLoading$(): Observable<boolean> {
+    return this.isLoading.asObservable();
+  }
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.handleShowLoader();
+  }
+
+  private handleShowLoader(): void {
+    this.router.events.subscribe((e) => {
+      if (e instanceof NavigationStart) {
+        this.isLoading.next(true);
+      }
+      if (
+        e instanceof NavigationEnd ||
+        e instanceof NavigationCancel ||
+        e instanceof NavigationError
+      ) {
+        this.isLoading.next(false);
+      }
+    });
+  }
+}
---
+++ src/app/components/loader/loader.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+@Component({
+  selector: 'app-loader',
+  standalone: true,
+  imports: [CommonModule, MatProgressSpinnerModule],
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.scss'],
+})
+export class LoaderComponent {}
