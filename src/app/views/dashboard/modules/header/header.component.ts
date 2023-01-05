b904618259e8aeab457cfaf1561556621cf2ec4a
Krystian Grzelak <krystian.grzelak@o2.pl>
2023-01-05 17:54:40 +0100

    feat: add logo

--- src/app/views/dashboard/modules/header/header.component.ts
+++ src/app/views/dashboard/modules/header/header.component.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { Router } from '@angular/router';
 import { AppState } from '@appStore/app.reducer';
 import { logout } from '@appStore/auth/auth.actions';
 import { selectUserInfo } from '@appStore/auth/auth.selectors';
@@ -19,7 +20,11 @@ import { Store } from '@ngrx/store';
 export class HeaderComponent {
   protected userInfo$ = this.store.select(selectUserInfo);
 
-  constructor(private store: Store<AppState>) {}
+  constructor(private store: Store<AppState>, private router: Router) {}
+
+  protected handleGoHome(): void {
+    this.router.navigate(['dashboard']);
+  }
 
   protected handleLogout(): void {
     this.store.dispatch(logout());
